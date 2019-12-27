36db94b975b1df93f88966ebc115320b7bee13d1
weixiangchen <1981468862@qq.com>
2019-12-27 17:38:25 +0800

    Refactor(web-code): add some dependecies and init the project
    Close #3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import Axios from 'axios';
+import VueAxios from 'vue-axios';
+import Qs from 'qs'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+Vue.use(VueAxios, Axios)
+Vue.prototype.$qs = Qs
+
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
