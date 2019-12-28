305728f043e8884130c77aac4c942943c204228f
weixiangchen <1981468862@qq.com>
2019-12-28 11:54:42 +0800

    Fix(web-code): solve the problem of store

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import LoginPage from '../pages/LoginPage'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [
+        {
+            path: '/login',
+            name: 'LoginPage',
+            component: LoginPage
+        }
     ]
 })
