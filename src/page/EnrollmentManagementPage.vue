5e3e26913e42ac5da8d7d646bcccfcfacb1622c2
陈为响 <1981468862@qq.com>
2020-01-01 14:00:37 +0800

    Feat(web-code): change the favicon of the website
    Ref #19

--- src/page/EnrollmentManagementPage.vue
+++ src/page/EnrollmentManagementPage.vue
@@ -1,15 +1,42 @@
 <template>
-  <div>
-    招生管理
-  </div>
+  <el-container>
+    <el-header>
+      <h2>{{ semester }}</h2>
+    </el-header>
+    <el-main class="main-in-enrollment">
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="未开放" name="first">
+          <unopen-classes></unopen-classes>
+        </el-tab-pane>
+        <el-tab-pane label="招生中" name="second">
+          <enrolling-classes></enrolling-classes>
+        </el-tab-pane>
+      </el-tabs>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
+import UnopenClasses from '../components/UnopenClasses'
+import EnrollingClasses from '../components/EnrollingClasses'
 export default {
-  name: 'EnrollmentManagementPage'
+  name: 'EnrollmentManagementPage',
+  components: {
+    UnopenClasses,
+    EnrollingClasses
+  },
+  data () {
+    return {
+      activeName: 'first',
+      semester: '第九期未来之星创新院'
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .main-in-enrollment {
+    padding: 0;
+    margin: 0;
+  }
 </style>
