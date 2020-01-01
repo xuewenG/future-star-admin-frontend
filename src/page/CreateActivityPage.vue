5948b51fa80c229c7d4c006cd9e44d8997708301
WinsomeWind <2424210985@qq.com>
2020-01-02 00:40:48 +0800

    Feat(web-code): finished the show-alumni-information page
    Close #48

--- src/page/CreateActivityPage.vue
+++ src/page/CreateActivityPage.vue
@@ -15,6 +15,7 @@
             <el-col :span="12">
               <el-form-item label="活动时间：">
                 <el-date-picker
+                  v-model="value1"
                   type="datetime"
                   placeholder="选择日期时间">
                 </el-date-picker>
