a22313b5e92400c22a0bce4b613f0293d71ef21f
weixiangchen <1981468862@qq.com>
2020-01-02 16:07:13 +0800

    Feat(course): finish the add-new-course-page
    Close #31

--- src/router/enrollment.js
+++ src/router/enrollment.js
@@ -1,8 +1,14 @@
 import ModifyEnrollmentClassInfoPage from '../page/ModifyEnrollmentClassInfoPage'
+import AddNewCoursePage from '../page/AddNewCoursePage'
 export default [
   {
     path: '/modify-enrollment-class-info',
     name: 'ModifyEnrollmentClassInfoPage',
     component: ModifyEnrollmentClassInfoPage
+  },
+  {
+    path: '/add-new-course',
+    name: 'AddNewCoursePage',
+    component: AddNewCoursePage
   }
 ]
