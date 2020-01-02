c8a3d3eb8ca36682ec8f8ef1da67a372c5356e62
weixiangchen <1981468862@qq.com>
2020-01-02 14:49:58 +0800

    Feat(web-code): finish the layout of modify-enrollment-class-info-page
    Ref #37

--- src/router/enrollment.js
+++ src/router/enrollment.js
@@ -1,2 +1,8 @@
+import ModifyEnrollmentClassInfoPage from '../page/ModifyEnrollmentClassInfoPage'
 export default [
+  {
+    path: '/modify-enrollment-class-info',
+    name: 'ModifyEnrollmentClassInfoPage',
+    component: ModifyEnrollmentClassInfoPage
+  }
 ]
