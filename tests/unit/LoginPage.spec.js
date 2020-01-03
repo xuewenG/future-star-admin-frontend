135f63180e5d946477e1af305c9046f3fb3facab
weixiangchen <1981468862@qq.com>
2020-01-03 21:40:02 +0800

    Chore(web-code): add stylelint to hook
    Close #57

--- tests/unit/LoginPage.spec.js
+++ tests/unit/LoginPage.spec.js
@@ -2,8 +2,11 @@ import { mount } from '@vue/test-utils'
 import LoginPage from '../../src/page/LoginPage'
 
 describe('LoginPage', () => {
+  const wrapper = mount(LoginPage)
+  test('快照测试', () => {
+    expect(wrapper.html()).toMatchSnapshot()
+  })
   test('是一个 Vue 实例', () => {
-    const wrapper = mount(LoginPage)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
