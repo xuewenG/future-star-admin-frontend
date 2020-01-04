ca98a8735372610eb842d5e6f2e23fc5c770235c
WinsomeWind <2424210985@qq.com>
2020-01-04 16:45:46 +0800

    Fix(web-code): fix a bug of login-page's test
    Ref #60

--- tests/unit/LoginPage.spec.js
+++ tests/unit/LoginPage.spec.js
@@ -3,10 +3,14 @@ import LoginPage from '../../src/page/LoginPage'
 
 describe('LoginPage', () => {
   const wrapper = mount(LoginPage)
-  test('快照测试', () => {
-    expect(wrapper.html()).toMatchSnapshot()
+  test('快照测试', async () => {
+    await expect(wrapper.html()).toMatchSnapshot()
   })
-  test('是一个 Vue 实例', () => {
-    expect(wrapper.isVueInstance()).toBeTruthy()
+  test('是一个 Vue 实例', async () => {
+    await expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+  test('按钮', async () => {
+    const button = wrapper.find('el-button')
+    await expect(button.text()).toBe('登录')
   })
 })
