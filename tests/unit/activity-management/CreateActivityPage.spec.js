294cb3fcfa6dda3f7f6198cacbee7cbc365b0950
WinsomeWind <2424210985@qq.com>
2020-01-04 23:00:05 +0800

    Fix(activity): fix a bug of activity-management's unit test
    Close #70

--- tests/unit/activity-management/CreateActivityPage.spec.js
+++ tests/unit/activity-management/CreateActivityPage.spec.js
@@ -1,8 +1,21 @@
-import { mount } from '@vue/test-utils'
+import { createLocalVue, mount } from '@vue/test-utils'
 import CreateActivityPage from '../../../src/page/activity-management/CreateActivityPage'
+import Vuex from 'vuex'
+import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
+import store from '../../../src/store'
+import router from '../../../src/router'
 
 describe('CreateActivityPage', () => {
-  const wrapper = mount(CreateActivityPage)
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+  localVue.use(ElementUI)
+  localVue.use(VueRouter)
+  const wrapper = mount(CreateActivityPage, {
+    store,
+    router,
+    localVue
+  })
   test('快照测试', async () => {
     await expect(wrapper.html()).toMatchSnapshot()
   })
