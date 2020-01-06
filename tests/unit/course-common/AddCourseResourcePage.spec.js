8164f39cf09755aabd131ae32eeb0df5dd6afa57
weixiangchen <1981468862@qq.com>
2020-01-06 18:13:34 +0800

    Test(course): add snapshot test to this component
    Ref #86

--- tests/unit/course-common/AddCourseResourcePage.spec.js
+++ tests/unit/course-common/AddCourseResourcePage.spec.js
@@ -1,25 +1,22 @@
 import { createLocalVue, mount } from '@vue/test-utils'
-import AddCoursePage from '../../../src/page/course-common/AddCoursePage'
+import AddCourseResourcePage from '../../../src/page/course-common/AddCourseResourcePage'
 import Vuex from 'vuex'
 import ElementUI from 'element-ui'
 import VueRouter from 'vue-router'
 import store from '../../../src/store'
 import router from '../../../src/router'
 
-describe('AddCoursePage', () => {
+describe('AddCourseResourcePage', () => {
   const localVue = createLocalVue()
   localVue.use(Vuex)
   localVue.use(ElementUI)
   localVue.use(VueRouter)
-  const wrapper = mount(AddCoursePage, {
+  const wrapper = mount(AddCourseResourcePage, {
     store,
     router,
     localVue
   })
   test('快照测试', async () => {
     await expect(wrapper.html()).toMatchSnapshot()
-  })
-  test('是一个 Vue 实例', async () => {
-    await expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
