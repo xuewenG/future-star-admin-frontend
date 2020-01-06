de0b9552ff1963fd8d88b5dd81d2773cef2e4c26
WinsomeWind <2424210985@qq.com>
2020-01-06 13:13:48 +0800

    Test(authority): add unit test of the authority management component
    Close #24

---
+++ tests/unit/home-children/AuthorityManagement.spec.js
@@ -0,0 +1,25 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import AuthorityManagement from '../../../src/components/home-children/AuthorityManagement'
+import Vuex from 'vuex'
+import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
+import store from '../../../src/store'
+import router from '../../../src/router'
+
+describe('AuthorityManagement', () => {
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+  localVue.use(ElementUI)
+  localVue.use(VueRouter)
+  const wrapper = mount(AuthorityManagement, {
+    store,
+    router,
+    localVue
+  })
+  test('快照测试', async () => {
+    await expect(wrapper.html()).toMatchSnapshot()
+  })
+  test('是一个 Vue 实例', async () => {
+    await expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
