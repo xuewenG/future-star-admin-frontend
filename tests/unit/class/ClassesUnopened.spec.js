e06a9911e69d76602b61d03e39885f45a2115e01
WinsomeWind <2424210985@qq.com>
2020-01-06 12:09:11 +0800

    Test(enrollment): add unit test of the unopen classes component
    Close #78

---
+++ tests/unit/class/ClassesUnopened.spec.js
@@ -0,0 +1,25 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import ClassesUnopened from '../../../src/components/class/ClassesUnopened'
+import Vuex from 'vuex'
+import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
+import store from '../../../src/store'
+import router from '../../../src/router'
+
+describe('ClassesUnopened', () => {
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+  localVue.use(ElementUI)
+  localVue.use(VueRouter)
+  const wrapper = mount(ClassesUnopened, {
+    store,
+    router,
+    localVue
+  })
+  test('快照测试', async () => {
+    await expect(wrapper.html()).toMatchSnapshot()
+  })
+  test('是一个 Vue 实例', async () => {
+    await expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
