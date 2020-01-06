5585b4b16d0e1b4da81487b27425f85368a60ec5
WinsomeWind <2424210985@qq.com>
2020-01-06 13:15:05 +0800

    Test(course): add unit test of the course managemnt component
    Close #81

---
+++ tests/unit/home-children/CourseManagement.spec.js
@@ -0,0 +1,25 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import CourseManagement from '../../../src/components/home-children/CourseManagement'
+import Vuex from 'vuex'
+import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
+import store from '../../../src/store'
+import router from '../../../src/router'
+
+describe('CourseManagement', () => {
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+  localVue.use(ElementUI)
+  localVue.use(VueRouter)
+  const wrapper = mount(CourseManagement, {
+    store,
+    router,
+    localVue
+  })
+  test('快照测试', async () => {
+    await expect(wrapper.html()).toMatchSnapshot()
+  })
+  test('是一个 Vue 实例', async () => {
+    await expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
