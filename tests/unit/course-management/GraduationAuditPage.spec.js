e97ed392c7f604638e2ba67a9fd1c5c3ff6a19ef
weixiangchen <1981468862@qq.com>
2020-01-06 19:40:00 +0800

    Test(course): add snapshot test to GraduationAuditPage
    Ref #94

---
+++ tests/unit/course-management/GraduationAuditPage.spec.js
@@ -0,0 +1,22 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import GraduationAuditPage from '../../../src/page/course-management/GraduationAuditPage'
+import Vuex from 'vuex'
+import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
+import store from '../../../src/store'
+import router from '../../../src/router'
+
+describe('GraduationAuditPage', () => {
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+  localVue.use(ElementUI)
+  localVue.use(VueRouter)
+  const wrapper = mount(GraduationAuditPage, {
+    store,
+    router,
+    localVue
+  })
+  test('快照测试', async () => {
+    await expect(wrapper.html()).toMatchSnapshot()
+  })
+})
