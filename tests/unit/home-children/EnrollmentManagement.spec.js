93caca6cdd99c563505ebedaa154018217d705f1
WinsomeWind <2424210985@qq.com>
2020-01-06 13:16:18 +0800

    Test(enrollment): add unit test of the enrollment management component
    Close #82

---
+++ tests/unit/home-children/EnrollmentManagement.spec.js
@@ -0,0 +1,25 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import EnrollmentManagement from '../../../src/components/home-children/EnrollmentManagement'
+import Vuex from 'vuex'
+import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
+import store from '../../../src/store'
+import router from '../../../src/router'
+
+describe('EnrollmentManagement', () => {
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+  localVue.use(ElementUI)
+  localVue.use(VueRouter)
+  const wrapper = mount(EnrollmentManagement, {
+    store,
+    router,
+    localVue
+  })
+  test('快照测试', async () => {
+    await expect(wrapper.html()).toMatchSnapshot()
+  })
+  test('是一个 Vue 实例', async () => {
+    await expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
