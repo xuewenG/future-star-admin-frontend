772d9747008d5102e4e7095bcc533f734e53099a
weixiangchen <1981468862@qq.com>
2020-01-06 18:41:32 +0800

    Test(course): add vue intsance test to this component
    Close #91

--- tests/unit/course-common/EditCourseInfoPage.spec.js
+++ tests/unit/course-common/EditCourseInfoPage.spec.js
@@ -19,4 +19,7 @@ describe('EditCourseInfoPage', () => {
   test('快照测试', async () => {
     await expect(wrapper.html()).toMatchSnapshot()
   })
+  test('是一个 Vue 实例', async () => {
+    await expect(wrapper.isVueInstance()).toBeTruthy()
+  })
 })
