a3c81fd43166604845aba74e44d9b932a0a6c08f
weixiangchen <1981468862@qq.com>
2020-01-06 18:46:30 +0800

    Test(course): add snapshot test to this component
    Ref #93

---
+++ tests/unit/course-common/StudentInClass.spec.js
@@ -0,0 +1,22 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import StudentInClass from '../../../src/page/course-common/StudentInClass'
+import Vuex from 'vuex'
+import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
+import store from '../../../src/store'
+import router from '../../../src/router'
+
+describe('StudentInClass', () => {
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+  localVue.use(ElementUI)
+  localVue.use(VueRouter)
+  const wrapper = mount(StudentInClass, {
+    store,
+    router,
+    localVue
+  })
+  test('快照测试', async () => {
+    await expect(wrapper.html()).toMatchSnapshot()
+  })
+})
